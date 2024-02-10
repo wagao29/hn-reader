68adb51d9f7d35bc9f75641c087f3cab1dcaed61
wagao <wagao1029@gmail.com>
2024-02-10 22:50:53 +0900

    Add Suspense to Card

--- app/[story]/page.tsx
+++ app/[story]/page.tsx
@@ -1,9 +1,11 @@
 import { Card } from '@/app/ui/cards';
 import { notFound } from 'next/navigation';
+import { Suspense } from 'react';
 import { fetchStories } from '../lib/data';
 import { PAGE_ITEM_SIZE, STORY_TYPE, StoryType } from '../lib/definition';
 import { validatePageNum } from '../lib/utils';
 import Pagination from '../ui/pagination';
+import { CardSkeleton } from '../ui/skeletons';
 
 export default async function Page({
   params,
@@ -19,15 +21,23 @@ export default async function Page({
 
   const currentPage = validatePageNum(searchParams?.page);
   const itemIds = await fetchStories(params.story);
-  const cards = itemIds
-    .slice((currentPage - 1) * PAGE_ITEM_SIZE, currentPage * PAGE_ITEM_SIZE)
-    .map((itemId) => <Card key={itemId} itemId={itemId} />);
   const totalPages = Math.ceil(itemIds.length / PAGE_ITEM_SIZE);
 
   return (
     <main className="my-10 flex flex-col items-center justify-center gap-10">
       <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
-        {cards}
+        {itemIds
+          .slice(
+            (currentPage - 1) * PAGE_ITEM_SIZE,
+            currentPage * PAGE_ITEM_SIZE,
+          )
+          .map((itemId) => {
+            return (
+              <Suspense key={itemId} fallback={<CardSkeleton />}>
+                <Card itemId={itemId} />
+              </Suspense>
+            );
+          })}
       </div>
       <Pagination totalPages={totalPages} />
     </main>
