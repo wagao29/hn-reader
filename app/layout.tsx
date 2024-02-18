e7647a2e314b7de8c4b81fbaa2ee5553ea5c5393
wagao <wagao1029@gmail.com>
2024-02-18 16:00:17 +0900

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import { inter } from '@/app/ui/fonts';
 import '@/app/ui/global.css';
+import { Metadata } from 'next';
 import { NavLinks } from './ui/nav-links';
+
+export const metadata: Metadata = {
+  title: 'HN Reader',
+  description: 'Hacker News reader built with Next.js App Router.',
+  metadataBase: new URL('https://next-hn-reader.vercel.app'),
+};
 
 export default function RootLayout({
   children,
